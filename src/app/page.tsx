ab4ea7cb6dbf4ae0121b253098c662bcd8fbe89a
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2025-03-26 20:29:48 -0300

    chore: fonts config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+import { Inter, Poppins } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  weight: ["400"],
+});
+
 export default function Home() {
   return (
-    <main>
-      <div>Hello world!</div>
+    <main className={`${inter.className} ${poppins.className}`}>
+      <div className="font-poppins text-9xl">Hello world!</div>
     </main>
   );
 }
