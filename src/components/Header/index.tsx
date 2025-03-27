4e73b47382a62d0f89d95647876177f826698d2b
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2025-03-27 20:17:53 -0300

    feat: paths header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,31 @@
+"use client";
+
 import logo from "@/assets/logo.png";
+import { routes } from "@/routes";
 import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import Button from "../Button";
 
 function Header() {
+  const pathname = usePathname(); // Obtendo o caminho atual
+
+  const isActive = (path: string) =>
+    pathname === path ? "text-blue-500" : "text-neutral-800"; // Altere para as cores que desejar
+
   return (
     <header className="flex items-center gap-16 py-6 px-12 w-full border-b-1 border-neutral-100">
       <Image src={logo} alt="Co-criar" />
 
       <nav className="flex-1">
         <ul className="flex items-center flex-1 gap-3 font-inter text-lg leading-relaxed ">
-          <li className="flex-1">Home</li>
-          <li className="flex-1">Sobre</li>
-          <li className="flex-1">Serviços</li>
-          <li className="flex-1">Eventos</li>
-          <li className="flex-1">Contato</li>
-          <li className="flex-1">Blog</li>
+          {routes.map((route) => (
+            <li key={route.path} className="flex-1">
+              <Link href={route.path} className={isActive(route.path)}>
+                {route.label}
+              </Link>
+            </li>
+          ))}
         </ul>
       </nav>
 
