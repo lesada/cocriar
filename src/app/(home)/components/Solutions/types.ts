d5c4c89b023c650ec6f466b4cc451ae61c1d2cbe
Lauren Leslie <68572174+lesada@users.noreply.github.com>
2025-04-08 14:27:20 -0300

    feat: solutions section (#5)
    * chore: organize tests
    
    * feat: section component
    
    * feat: solutions page
    
    * fix: remove console log

---
+++ src/app/(home)/components/Solutions/types.ts
@@ -0,0 +1,20 @@
+import { IconName } from "lucide-react/dynamic";
+
+export type TColors = {
+  detach: string;
+  border: string;
+  icon: string;
+};
+
+export type TSolutionsItem = {
+  icon: IconName;
+  title: string;
+  content: string;
+  detach: boolean;
+};
+
+export type TSolutionsSection = {
+  name: string;
+  items: TSolutionsItem[];
+  colors?: TColors;
+};
