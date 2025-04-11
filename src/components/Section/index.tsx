381a286fa2f5a427db4b24201755af5f5bd99118
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2025-04-11 11:15:39 -0300

    fix: mobile padding

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -6,7 +6,7 @@ type SectionProps = {
 
 function Section({ tag, children, className, ...rest }: SectionProps) {
   return (
-    <section {...rest} className={`flex flex-col px-28 py-12 ${className}`}>
+    <section {...rest} className={`flex flex-col p-12 lg:px-28  ${className}`}>
       <h4 className="uppercase font-inter text-sm text-purple-900 bg-purple-50 rounded-lg p-2 w-fit font-medium mb-8">
         {tag}
       </h4>
