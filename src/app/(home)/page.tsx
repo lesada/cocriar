c10cf711b565e5883bd3f95ab55ec4a7cb9f0342
Lauren Leslie <68572174+lesada@users.noreply.github.com>
2025-04-14 09:54:37 -0300

    feat: carousel (#6)
    * feat: carousel
    
    * fix: default font poppins

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,11 @@
 import Solutions from "./components/Solutions";
+import Testimonials from "./components/Testimonials";
 
 export default function Home() {
   return (
     <main>
       <Solutions />
+      <Testimonials />
     </main>
   );
 }
