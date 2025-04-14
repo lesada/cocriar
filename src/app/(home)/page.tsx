32ae81c0dcc6dc5f09fa8e331744ca31b7d91f04
Lauren Leslie <68572174+lesada@users.noreply.github.com>
2025-04-14 09:59:01 -0300

    feat: articles blog (#8)

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,4 @@
+import Articles from "./components/Articles";
 import Solutions from "./components/Solutions";
 import Testimonials from "./components/Testimonials";
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <main>
       <Solutions />
+      <Articles />
       <Testimonials />
     </main>
   );
