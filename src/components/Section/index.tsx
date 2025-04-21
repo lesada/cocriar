bbbf81f25f9e60543216274f622b1a419cd1cb89
ericardmatosz <ericardmatosz@gmail.com>
2025-04-21 18:41:54 -0300

    feat: adjust layout by limiting width and centering components

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -6,11 +6,13 @@ type SectionProps = {
 
 function Section({ tag, children, className, ...rest }: SectionProps) {
   return (
-    <section {...rest} className={`flex flex-col p-12 lg:px-28  ${className}`}>
-      <h4 className="uppercase font-inter text-sm text-purple-900 bg-purple-50 rounded-lg p-2 w-fit font-medium mb-8">
-        {tag}
-      </h4>
-      {children}
+    <section {...rest} className={`${className}`}>
+      <div className="flex flex-col p-12 lg:px-28 max-w-[81rem] mx-auto">
+        <h4 className="uppercase font-inter text-sm text-purple-900 bg-purple-50 rounded-lg p-2 w-fit font-medium mb-8">
+          {tag}
+        </h4>
+        {children}
+      </div>
     </section>
   );
 }
