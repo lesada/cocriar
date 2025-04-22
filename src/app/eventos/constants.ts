258b5920609f8570727812473412bdfbe82be93f
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2025-04-22 16:48:01 -0300

    feat: events

---
+++ src/app/eventos/constants.ts
@@ -0,0 +1,30 @@
+export const events = [
+	{
+		title: "5 Conteúdos que você precisa ler em 2023",
+		text: "É um ecossistema vivo de criatividade e inovação focado em resultados. A Comunidade digital possui uma gama de serviços e vantagens para seus membros como mentorias, cursos, acessos a espaços coworkings.",
+		tag: "Tecnologia",
+		image: "https://placehold.co/300x200",
+		slug: "5-conteudos",
+	},
+	{
+		title: "Cybersegurança em 2023: Desafios",
+		text: "É um ecossistema vivo de criatividade e inovação focado em resultados. A Comunidade digital possui uma gama de serviços e vantagens para seus membros como mentorias, cursos, acessos a espaços coworkings.",
+		tag: "Tecnologia",
+		image: "https://placehold.co/300x200",
+		slug: "cyber",
+	},
+	{
+		title: "Realidade Virtual: Além dos Jogos",
+		text: "É um ecossistema vivo de criatividade e inovação focado em resultados. A Comunidade digital possui uma gama de serviços e vantagens para seus membros como mentorias, cursos, acessos a espaços coworkings.",
+		tag: "Tecnologia",
+		image: "https://placehold.co/300x200",
+		slug: "realidade",
+	},
+	{
+		title: "A Revolução da Inteligência Artificial",
+		text: "É um ecossistema vivo de criatividade e inovação focado em resultados. A Comunidade digital possui uma gama de serviços e vantagens para seus membros como mentorias, cursos, acessos a espaços coworkings.",
+		tag: "Tecnologia",
+		image: "https://placehold.co/300x200",
+		slug: "revolucao",
+	},
+];
