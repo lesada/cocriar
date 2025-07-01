35c983f4d3932d5d9e721cbfb4d17544dea5a8f9
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2025-07-01 10:41:31 -0300

    fix: sidebar expand

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -19,23 +19,29 @@ function Sidebar() {
 		pathname === path ? "bg-blue-100" : "";
 
 	return (
-		<nav className="top-0 z-50 sticky flex flex-col justify-center items-center gap-16 bg-neutral-0 px-4 py-6 border-neutral-100 border-r-1 w-[18rem] h-screen">
+		<nav
+			className={`top-0 z-50 sticky flex flex-col justify-center items-center gap-16 bg-neutral-0 px-4 py-6 border-neutral-100 border-r-1 ${isMenuOpen ? "w-[18rem]" : "w-fit"} h-screen`}
+		>
 			<div className="flex items-center gap-4">
 				<button
-					className="flex justify-center items-center p-2"
+					className="flex justify-center items-center p-2 cursor-pointer"
 					onClick={toggleMenu}
 					type="button"
 				>
 					<Icon icon="material-symbols:menu-rounded" width={32} height={32} />
 				</button>
-				<Image
-					src={logo}
-					alt="Co-criar"
-					className="shrink-0"
-					width={120}
-					height={50}
-				/>
-				<div className="p-2 w-8" />
+				{isMenuOpen && (
+					<>
+						<Image
+							src={logo}
+							alt="Co-criar"
+							className="shrink-0"
+							width={120}
+							height={50}
+						/>
+						<div className="p-2 w-8" />
+					</>
+				)}
 			</div>
 
 			<ul className="flex flex-col flex-1 items-center gap-1 w-full font-inter text-lg leading-relaxed">
@@ -46,8 +52,7 @@ function Sidebar() {
 							className={`flex items-center gap-2 px-6 py-4 rounded-lg w-full ${activeClass(route.path)}`}
 						>
 							<Icon icon={route.icon} width={24} height={24} />
-
-							{route.label}
+							{isMenuOpen && <span>{route.label}</span>}
 						</Link>
 					</li>
 				))}
