29379dbeb4e38cbf74b014fb85549233a3400e0e
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2025-09-15 17:31:22 -0300

    feat: delete article handler

--- src/api/mocks/handlers/index.ts
+++ src/api/mocks/handlers/index.ts
@@ -1,13 +1,11 @@
-import { articlesMock } from "@/api/requests/articles/mock";
 import { summaryMock } from "@/api/requests/summary/mock";
 import { http, HttpResponse } from "msw";
+import { articlesMockHandler } from "./articles";
 
 export const handlers = [
 	http.get("/summary", () => {
 		return HttpResponse.json(summaryMock);
 	}),
 
-	http.get("/articles", () => {
-		return HttpResponse.json(articlesMock);
-	}),
+	...articlesMockHandler,
 ];
