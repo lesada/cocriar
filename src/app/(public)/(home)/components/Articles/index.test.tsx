f3030533965a08d28ab1d781bfc333393b738917
Lauren Leslie Ferreira <laurenlesliesilva@gmail.com>
2025-10-03 23:24:09 -0300

    tests: fix server

--- src/app/(public)/(home)/components/Articles/index.test.tsx
+++ src/app/(public)/(home)/components/Articles/index.test.tsx
@@ -1,17 +1,26 @@
-import { render, screen } from "@testing-library/react";
-import { describe, expect, test } from "vitest";
+import { renderWithClient } from "@/_tests/utils";
+import { server } from "@/api/mocks/server";
+import { articlesMock } from "@/api/requests/articles/mock";
+import { screen } from "@testing-library/react";
+import { afterAll, beforeAll, describe, expect, test } from "vitest";
 import Articles from ".";
 
 describe("home > components > Articles", () => {
+		beforeAll(() => {
+		server.listen();
+	});
+
+	afterAll(() => server.close());
+	
 	test("render articles", async () => {
-		render(<Articles />);
+		renderWithClient(<Articles />);
 
 		const tag = screen.getByText("Blog Co-criar");
 
 		expect(tag).toBeVisible();
 
-		const titleFirstArticle = screen.getAllByText(
-			"5 Conteúdos que você precisa ler em 2023",
+		const titleFirstArticle = await screen.findAllByText(
+			articlesMock.articles[0].title,
 		);
 
 		expect(titleFirstArticle[0]).toBeVisible();
--- src/app/(public)/(home)/index.test.tsx
+++ src/app/(public)/(home)/index.test.tsx
@@ -1,9 +1,16 @@
-import { render } from "@testing-library/react";
-import { describe, test } from "vitest";
+import { renderWithClient } from "@/_tests/utils";
+import { server } from "@/api/mocks/server";
+import { afterAll, beforeAll, describe, test } from "vitest";
 import Home from "./page";
 
 describe("home ", () => {
+	beforeAll(() => {
+		server.listen();
+	});
+
+	afterAll(() => server.close());
+	
 	test("render home", async () => {
-		render(<Home />);
+		renderWithClient(<Home />);
 	});
 });
